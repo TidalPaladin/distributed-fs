79e13f7b994bf0e4881af438950c798bb523f8b8
sreekara95 <57969649+sreekara95@users.noreply.github.com>
2019-12-02 23:12:06 -0600

    Add files via upload
    My Implementation method.

---
+++ progstart.ps1
@@ -0,0 +1,12 @@
+﻿cd C:\Users\sreek\eclipse-workspace\AOS_PROJ3\src
+rm *.class
+javac *.java
+Start-Process java -ArgumentList('metaserver',0)
+Start-Process java -ArgumentList('Client',0)
+Start-Process java -ArgumentList('Client',1)
+Start-Process java -ArgumentList('Server',0)
+Start-Process java -ArgumentList('Server',1)
+Start-Process java -ArgumentList('Server',2)
+Start-Process java -ArgumentList('Server',3)
+Start-Process java -ArgumentList('Server',4)
+Start-Process java -ArgumentList('Server',5)
